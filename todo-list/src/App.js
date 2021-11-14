e10fe88bf37d57e4647d30f5795ddfbccc7493cb
ShujaAazik <aazikshuja@hotmail.com>
2021-11-14 23:45:37 +0000

    Added todo Functionality but the code doesnt work still

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
-import reactDom from "react-dom";
+/** @format */
+
+import React, { useState } from 'react';
+import reactDom from 'react-dom';
 import './App.css';
 
 function App() {
-  return (
+	const [text, setText] = useState('');
+	const [list,setList] = useState([]);
+	return (
 		<div>
 			<div className='Input-container'>
 				<h1>ToDo List</h1>
-				<input type='text'></input>
-				<button>Add</button>
+				<input type='text' onChange={(e) => {setText(e.target.value)}} value={text} />
+				<button onClick={()=>{setList(list.concat(text))}}>Add</button>
 			</div>
-			<div className='Todo-list-container'></div>
+			<div className='Todo-list-container'><Addtolist value = {list}/></div>
 		</div>
 	);
 }
 
-
+function Addtolist(props){
+	return(
+		<ul>
+			{
+				props.value.map(e => <li>{e.target.value}</li>)
+			}
+		</ul>
+	);
+}
 
 export default App;
