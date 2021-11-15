750077a0f6da93569c9f373187c4655517e97351
ShujaAazik <aazikshuja@hotmail.com>
2021-11-15 23:53:29 +0000

    Errors Fixed and Todo list work with basic functions

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -12,7 +12,7 @@ function App() {
 			<div className='Input-container'>
 				<h1>ToDo List</h1>
 				<input type='text' onChange={(e) => {setText(e.target.value)}} value={text} />
-				<button onClick={()=>{setList(list.concat(text))}}>Add</button>
+				<button onClick={()=>{setList(list.concat(text));setText("");}}>Add</button>
 			</div>
 			<div className='Todo-list-container'><Addtolist value = {list}/></div>
 		</div>
@@ -23,7 +23,7 @@ function Addtolist(props){
 	return(
 		<ul>
 			{
-				props.value.map(e => <li>{e.target.value}</li>)
+				props.value.map(e => <li>{e}</li>)
 			}
 		</ul>
 	);
