6503366a5d95418d25d6eae568f6090b54bfce40
ShujaAazik <aazikshuja@hotmail.com>
2021-11-19 23:22:38 +0000

    added uniqe identifier for each todoList

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,18 +1,23 @@
 /** @format */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import reactDom from 'react-dom';
 import './App.css';
 
 function App() {
 	const [text, setText] = useState('');
 	const [list,setList] = useState([]);
+
+	useEffect(() => {
+		console.log("Shuja")
+	}, [list,text]);
+
 	return (
 		<div>
 			<div className='Input-container'>
 				<h1>ToDo List</h1>
 				<input type='text' onChange={(e) => {setText(e.target.value)}} value={text} />
-				<button onClick={()=>{setList(list.concat(text));setText("");}}>Add</button>
+				<button onClick={()=>{setList(list.concat({key: new Date().getUTCDate(), value: text}));setText("");}}>Add</button>
 			</div>
 			<div className='Todo-list-container'><Addtolist value = {list}/></div>
 		</div>
@@ -24,7 +29,12 @@ function Addtolist(props){
 		<ul>
 			<div className='Todo-list-element'>
 				{props.value.map((e) => (
-					<li>{e}<button onClick={()=>(props.value.splice(props.value.indexOf(e)))}>X</button></li>
+					<li>
+						{e.value}
+						<button onClick={(e) => props.value.splice(props.value.indexOf(e))}>
+							X
+						</button>
+					</li>
 				))}
 			</div>
 		</ul>
