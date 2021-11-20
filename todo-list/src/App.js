1b4965066091cc7b7df9f13b8e615477ddbefe9c
ShujaAazik <aazikshuja@hotmail.com>
2021-11-20 23:51:34 +0000

    Adding Null Todos Removed

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,37 +1,55 @@
 /** @format */
 
 import React, { useState, useEffect } from 'react';
-import reactDom from 'react-dom';
 import './App.css';
 
 function App() {
 	const [text, setText] = useState('');
-	const [list,setList] = useState([]);
+	const [list, setList] = useState([]);
 
 	useEffect(() => {
-		console.log("Shuja")
-	}, [list,text]);
+		console.log('Shuja');
+	});
 
 	return (
 		<div>
 			<div className='Input-container'>
 				<h1>ToDo List</h1>
-				<input type='text' onChange={(e) => {setText(e.target.value)}} value={text} />
-				<button onClick={()=>{setList(list.concat({key: new Date().getUTCDate(), value: text}));setText("");}}>Add</button>
+				<input
+					type='text'
+					onChange={(e) => {
+						setText(e.target.value);
+					}}
+					value={text}
+				/>
+				<button
+					onClick={() => {
+						if (text.length > 0) {
+							setList(
+								list.concat({ key: new Date().getUTCDate(), value: text })
+							);
+						}
+						setText('');
+					}}>
+					Add
+				</button>
 			</div>
-			<div className='Todo-list-container'><Addtolist value = {list}/></div>
+			<div className='Todo-list-container'>
+				<Addtolist value={list} />
+			</div>
 		</div>
 	);
 }
 
-function Addtolist(props){
+function Addtolist(props) {
 	return (
 		<ul>
 			<div className='Todo-list-element'>
-				{props.value.map((e) => (
-					<li>
-						{e.value}
-						<button onClick={(e) => props.value.splice(props.value.indexOf(e))}>
+				{props.value.map((todo, index) => (
+					<li key={index}>
+						{todo.value}
+						<button
+							onClick={() => props.value.splice(props.value.indexOf(index))}>
 							X
 						</button>
 					</li>
